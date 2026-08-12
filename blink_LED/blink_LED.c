1de98b709f2dad57b0b5fefe41d9eef0a8d7578e
arthuryoung <arthur22444@gmail.com>
2026-08-12 11:02:35 +0100

    Added more LEDS to represent the different axes and got the limit switches to switch off the LEDs rahter than switching them on

--- blink_LED/blink_LED.c
+++ blink_LED/blink_LED.c
@@ -13,16 +13,25 @@ int main()
 
     // Initialise the LED outputs
     int spindleMotorPin = initialise_led(0);
+    int xAxisStepPin = initialise_led(7);
+    int yAxisStepPin = initialise_led(8);
+    int zAxisStepPin = initialise_led(9);
 
     while (true)
     {
         if (monitor_limit_switches() == 1)
         {
-            set_led(spindleMotorPin, 1);
+            set_led(spindleMotorPin, 0);
+            set_led(xAxisStepPin, 0);
+            set_led(yAxisStepPin, 0);
+            set_led(zAxisStepPin, 0);
         }
         else
         {
-            set_led(spindleMotorPin, 0);
+            set_led(spindleMotorPin, 1);
+            set_led(xAxisStepPin, 1);
+            set_led(yAxisStepPin, 1);
+            set_led(zAxisStepPin, 1);
         }
     }
 }
